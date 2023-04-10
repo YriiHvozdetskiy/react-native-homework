2ab0d571e4c5b538c079205eca2f1c53564fd180
rasty <hvozdetskiyyrii@gmail.com>
2023-04-10 15:08:52 +0300

    refactor

--- App.js
+++ App.js
@@ -1,46 +1,12 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
-import {Header} from '@components/Header';
-import {useFonts} from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
-import {useCallback} from 'react';
-
-SplashScreen.preventAutoHideAsync();
-
+import {AppNavigator} from './src/navigation';
+import {View} from 'react-native';
 
 export default function App() {
-   const [fontsLoaded] = useFonts({
-      'Roboto-Regular': require('./src/assets/fonts/Roboto-Regular.ttf'),
-      'Roboto-Medium': require('./src/assets/fonts/Roboto-Medium.ttf'),
-      'Roboto-Bold': require('./src/assets/fonts/Roboto-Bold.ttf'),
-      'Montserrat-Medium': require('./src/assets/fonts/Montserrat-Medium.ttf'),
-   });
-   const onLayoutRootView = useCallback(async () => {
-      if (fontsLoaded) {
-         await SplashScreen.hideAsync();
-      }
-   }, [fontsLoaded]);
-
-   if (!fontsLoaded) return null;
 
    return (
-      <View style={styles.container} onLayout={onLayoutRootView}>
-         <Text style={styles.text}>Ops hello</Text>
-         <Header/>
-         <StatusBar style="auto"/>
+      <View>
+         <AppNavigator/>
       </View>
    );
 }
 
-const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-   },
-   text: {
-      // fontFamily: 'Montserrat-Medium',
-      fontFamily: 'Roboto-Bold',
-   },
-});
--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -1,127 +1,31 @@
-// import React from 'react';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-//
-// import ForgotPassword from '../screens/ForgotPassword';
-// import Welcome from '../screens/Welcome';
-// import Login from '../screens/Login';
-// import Registration from '../screens/Registration';
-// import ScanOrChoose from '../screens/ScanOrChoose';
-// import Catalog from '../screens/Catalog';
-// import Profile from '../screens/Profile';
-// import Stores from '../screens/Stores';
-// import TakePhoto from '../screens/TakePhoto';
-// import Wishlist from '../screens/Wishlist';
-// import NoMatchFound from '../screens/NoMatchFound';
-// import Goods from '../screens/Goods';
-// import ProLabel from '../screens/ProLabel';
-// import SimilarProducts from '../screens/SimilarProducts';
-// import HowToKnit from '../screens/HowToKnit';
-//
-// import CustomHeader from '../components/CustomHeader';
-//
-// import {
-//   HomeIcon,
-//   LikesIcon,
-//   PhotoIcon,
-//   ShopIcon,
-//   ProfileIcon
-// } from '../assets/icons';
+import {Header} from '@components/Header';
+import {Container} from './styled';
+import {useFonts} from 'expo-font';
+import {useCallback} from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import {Text} from 'react-native';
 
-import {LoginScreen} from '@screens/LoginScreen';
+SplashScreen.preventAutoHideAsync();
 
-// const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+export const AppNavigator = () => {
+   const [fontsLoaded] = useFonts({
+      'Roboto-Regular': require('../assets/fonts/Roboto-Regular.ttf'),
+      'Roboto-Medium': require('../assets/fonts/Roboto-Medium.ttf'),
+      'Roboto-Bold': require('../assets/fonts/Roboto-Bold.ttf'),
+      'Montserrat-Medium': require('../assets/fonts/Montserrat-Medium.ttf'),
+   });
+   const onLayoutRootView = useCallback(async () => {
+      if (fontsLoaded) {
+         await SplashScreen.hideAsync();
+      }
+   }, [fontsLoaded]);
 
-const MainTabNavigator = ({navigation}) => {
+   if (!fontsLoaded) return null;
+
    return (
-      <Tab.Navigator
-         screenOptions={{
-            headerShown: false,
-            tabBarInactiveTintColor: "#A8A8A8",
-            tabBarActiveTintColor: '#E5097F',
-            tabBarShowLabel: false,
-            tabBarStyle: {
-               backgroundColor: '#F2F9FC',
-               borderTopColor: '#ffffff',
-               elevation: 0,
-               shadowOpacity: 0,
-               borderTopWidth: 2,
-            },
-         }}
-         initialRouteName="Catalog"
-      >
-         <Tab.Screen
-            name="Profile"
-            component={Profile}
-            options={{
-               tabBarIcon: ({color, size}) => (
-                  <ProfileIcon color={color}/>
-               ),
-            }}
-         />
-         <Tab.Screen
-            name="LoginScreen"
-            component={LoginScreen}
-            // options={{
-            //     tabBarIcon: ({ color, size }) => (
-            //         <HomeIcon color={color} />
-            //     )
-            // }}
-         />
-      </Tab.Navigator>
-   );
-};
-
-// const AppNavigator = ({ userToken }) => {
-//     return (
-//         <Stack.Navigator
-//             screenOptions={{
-//                 headerShown: false,
-//                 animation: "none"
-//             }}
-//         >
-//             {userToken ? (
-//                 <>
-//                     <Stack.Screen
-//                         name="Main"
-//                         component={MainTabNavigator}
-//                     />
-//                     <Stack.Screen
-//                         name="Welcome"
-//                         component={Welcome}
-//                     />
-//                     <Stack.Screen
-//                         name="ScanOrChoose"
-//                         component={ScanOrChoose}
-//                     />
-//                     <Stack.Screen
-//                         name="NoMatchFound"
-//                         component={NoMatchFound}
-//                     />
-//                 </>
-//             ) : (
-//                 <>
-//                     <Stack.Screen
-//                         name="Welcome"
-//                         component={Welcome}
-//                     />
-//                     <Stack.Screen
-//                         name="Registration"
-//                         component={Registration}
-//                     />
-//                     <Stack.Screen
-//                         name="Login"
-//                         component={Login}
-//                     />
-//                     <Stack.Screen
-//                         name="ForgotPassword"
-//                         component={ForgotPassword}
-//                     />
-//                 </>
-//             )}
-//         </Stack.Navigator>
-//     );
-// };
-
-// export default AppNavigator;
+      <Container onLayout={onLayoutRootView}>
+         <Header/>
+         {/*<Text>test</Text>*/}
+      </Container>
+   )
+}
